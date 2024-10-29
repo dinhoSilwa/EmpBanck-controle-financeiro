ae35df1f0053b40ac5d13848c18d25566ff1ddce
claudio silva <claudiosilva.one@gmail.com>
2024-10-29 14:49:20 -0300

    feat: create service to manage queries with TanStack

---
+++ src/service/transactions/service.ts
@@ -0,0 +1,10 @@
+import type { AxiosInstance } from "axios";
+import type { FinancialRecords } from "../../hooks/useForm/validationForms";
+import { TransactionPostMethod } from "./methods/post";
+
+export class TransactionService {
+  createTransaction = async (
+    api: AxiosInstance,
+    data: FinancialRecords
+  ): Promise<void> => await TransactionPostMethod(api, data);
+}
