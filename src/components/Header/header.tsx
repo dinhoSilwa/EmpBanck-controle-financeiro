7bf396695c8a4025f8f8e80ba1d0f3442cbc865e
claudio silva <claudiosilva.one@gmail.com>
2024-11-01 15:21:20 -0300

    fix : new transaction button

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,17 +1,20 @@
 import  conpanyLogo from "../../../public/logo horizontal.png"
 import { ModalManageStore } from "../../store/modalStore/modal"
 
+const buttonClasses =
+  "px-4 h-12 rounded-sm bg-secondary text-white hover:bg-secondary/75";
+
+
 export const Header = () =>{
 
   const {openModal}= ModalManageStore()
   return(
-    <header>
+    <header className="w-[80%] h-32 flex justify-between items-center">
       <figure>
-        <img src={conpanyLogo} alt={"logo da empresa"} />
+        <img src={conpanyLogo} alt={"logo da empresa"} className="w-56" />
       </figure>
-
-      <button onClick={() =>openModal('newTransaction')}>
-        <span>Nona Transação</span>
+      <button onClick={() =>openModal('newTransaction')} className={buttonClasses}>
+        <span>Nova Transação</span>
       </button>
     </header>
   )
